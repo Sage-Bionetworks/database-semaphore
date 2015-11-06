adfa3dac253efad8859242603369e778fde28af8
john-hill <john.hill@sagebase.org>
2015-11-05 18:35:51 -0800

    added missing files

---
+++ src/main/resources/schema/writeread/refreshWriteLock.ddl.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE refreshWriteLock(IN lockKey VARCHAR(256), IN tokenIn VARCHAR(256), IN timeoutSec INT(4))
+BEGIN
+	DECLARE writeLockToken VARCHAR(256);
+	DECLARE precursorToken VARCHAR(256);
+	DECLARE newExpiresOn TIMESTAMP;
+	
+	/* Lock on master and get the state of the write an precursor tokens */
+	CALL lockOnWriteReadMaster(lockKey, writeLockToken, precursorToken);
+	/* refresh the lock */
+	SET newExpiresOn = CURRENT_TIMESTAMP + INTERVAL timeoutSec SECOND;
+	UPDATE WRITE_READ_MASTER SET EXPIRES_ON = newExpiresOn WHERE LOCK_KEY = lockKey AND WRITE_LOCK_TOKEN = tokenIn;
+	/*Count the rows affected by the update*/
+	SELECT ROW_COUNT() AS RESULT;
+	COMMIT;
+END;
