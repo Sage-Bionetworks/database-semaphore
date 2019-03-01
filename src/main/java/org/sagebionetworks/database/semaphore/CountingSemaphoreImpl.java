949cf68862bcc0ecba29746becc6efc883330c9b
john-hill <john.hill@sagebase.org>
2019-03-01 14:35:24 -0800

    remove unused manager

--- src/main/java/org/sagebionetworks/database/semaphore/CountingSemaphoreImpl.java
+++ src/main/java/org/sagebionetworks/database/semaphore/CountingSemaphoreImpl.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * <p>
@@ -57,11 +56,6 @@ public class CountingSemaphoreImpl implements CountingSemaphore {
 	private static final String PROCEDURE_DDL_SQL_TEMPLATE = "schema/%s.ddl.sql";
 	private static final String PROCEDURE_EXITS_TEMPLATE = "PROCEDURE %s already exists";
 	
-//	/*
-//	 * All operations for this class require a READ_COMMITED transaction
-//	 * isolation level.
-//	 */
-//	TransactionTemplate readCommitedTransactionTemplate;
 	private JdbcTemplate jdbcTemplate;
 
 	/**
@@ -76,19 +70,11 @@ public class CountingSemaphoreImpl implements CountingSemaphore {
 	 *            The singleton transaction manager.
 	 * 
 	 */
-	public CountingSemaphoreImpl(DataSource dataSourcePool,
-			PlatformTransactionManager transactionManager) {
+	public CountingSemaphoreImpl(DataSource dataSourcePool) {
 		if (dataSourcePool == null) {
 			throw new IllegalArgumentException("DataSource cannot be null");
 		}
 		jdbcTemplate = new JdbcTemplate(dataSourcePool);
-//		/*
-//		 * All operations for this class require a READ_COMMITED transaction
-//		 * isolation level.
-//		 */
-//		readCommitedTransactionTemplate = Utils
-//				.createReadCommitedTransactionTempalte(transactionManager,
-//						"CountingSemaphoreImpl");
 
 		// Create the tables
 		this.jdbcTemplate.update(Utils
